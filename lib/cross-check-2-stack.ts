80f3f9e55a93a89498f69cd2ea25a942d8faf006
Muhammad Karimov <karimov.shermukhammad.tech@gmail.com>
2025-02-21 17:27:14 +0500

    created & implemented two lambdas

--- lib/cross-check-2-stack.ts
+++ lib/cross-check-2-stack.ts
@@ -1,16 +1,30 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
 
 export class CrossCheck2Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const productListFunction = new lambda.Function(this, "ProductListFunction", {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      code: lambda.Code.fromAsset("lambda"),
+      handler: "getProductListLambda.handler",
+    });
+    
+    const productByIdFunction = new lambda.Function(this, "ProductByIdFunction", {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      code: lambda.Code.fromAsset("lambda"),
+      handler: "getProductByIdLambda.handler",
+    });
+    
+    const api = new apigateway.RestApi(this, "ProductServiceAPI");
+    
+    const getProducts = api.root.addResource("products");
+    getProducts.addMethod("GET", new apigateway.LambdaIntegration(productListFunction));
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CrossCheck2Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const productById = getProducts.addResource("{productId}");
+    productById.addMethod("GET", new apigateway.LambdaIntegration(productByIdFunction));
   }
 }
