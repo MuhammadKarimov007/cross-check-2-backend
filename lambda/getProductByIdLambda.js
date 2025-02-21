572ee5fc285efae19aed886859d1277d3f8fb21b
Muhammad Karimov <karimov.shermukhammad.tech@gmail.com>
2025-02-21 20:14:32 +0500

    added lambda dir

---
+++ lambda/getProductByIdLambda.js
@@ -0,0 +1,23 @@
+const products = require("./products");
+
+exports.handler = async (event) => {
+  const productId = event.pathParameters.productId;
+  const product = products.find((p) => p.id === productId);
+
+  if (!product) {
+    return {
+      statusCode: 404,
+      body: JSON.stringify({ message: "Product not found" }),
+    };
+  }
+
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+    },
+    body: JSON.stringify(product),
+  };
+};
+
